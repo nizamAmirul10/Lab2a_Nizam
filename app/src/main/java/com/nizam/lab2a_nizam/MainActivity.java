096112152842c08cca0ad32e0ab6a42e854f34d0
SHAZLIN NIZAM AMIRUL <shazlinnizam@gmail.com>
2022-11-19 07:51:51 +0800

    Initial commit

---
+++ app/src/main/java/com/nizam/lab2a_nizam/MainActivity.java
@@ -0,0 +1,40 @@
+package com.nizam.lab2a_nizam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        String[] values = new String[]{
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"
+
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                 android.R.layout.simple_expandable_list_item_1, android.R.id.text1, values);
+            listView.setAdapter(adapter);
+
+    }
+}
